9e859fb94845ea774a8c2752e0e7c95a5d7c5337
Joseph Kenyon <36447805+dedejoe@users.noreply.github.com>
2019-09-18 18:02:13 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+      uri: 'mongodb+srv://dedejoe:0FeGwU2ymY1pW8oL@cluster0-crrcc.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+      
+  }
+};
